cb7aa70df8b1a484a271fd7d777ef59d3eac9a1c
Sabas Segovia <sasegovia@ucdavis.edu>
2022-09-24 13:59:09 -0700

    feat: replacing inneficient sprite scroller

--- script.js
+++ script.js
@@ -8,11 +8,28 @@ const CANVAS_HEIGHT = canvas.height = 600;
 //built in image class constructor
 const playerImage = new Image();
 playerImage.src = 'shadow_dog.png';
+const spriteWidth = 575;
+const spriteHeight = 523;
+let frameX = 0;
+let frameY = 0;
+//to slow down animateion, staggerframe controls the frame speed
+let gameFrame = 0;
+let staggerFrame = 5
+
 
 function animate() {
-  //clears the rectangle field using our global measurements
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  ctx.fillRect(50, 50, 100, 100);
+  //image: the image we are spriting over
+  //sx/sy: use traversing the source image by column/row
+  //sw/sh: use selecting how much from the photo to crop out
+  //dx/dy: starting point to grab the image from
+  //dw/dh: how much of the image to grab in relation to the dx/dy
+  ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+  if (gameFrame % staggerFrame === 0) {
+    if (frameX < 6) frameX++;
+    else frameX = 0;
+  }
+  gameFrame++;
   requestAnimationFrame(animate);
 };
 animate();
