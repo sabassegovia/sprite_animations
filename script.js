33757700d9c416dfc16661e47d3032f9c643ef7d
Sabas Segovia <sasegovia@ucdavis.edu>
2022-09-24 13:30:23 -0700

    feat: added png photo and animation loop

--- script.js
+++ script.js
@@ -5,4 +5,14 @@ const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
 
+//built in image class constructor
+const playerImage = new Image();
+playerImage.src = 'shadow_dog.png';
 
+function animate() {
+  //clears the rectangle field using our global measurements
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  ctx.fillRect(50, 50, 100, 100);
+  requestAnimationFrame(animate);
+};
+animate();
