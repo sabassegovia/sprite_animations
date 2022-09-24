8731eea2faa23e1728513657d7f4568428eeede5
Sabas Segovia <sasegovia@ucdavis.edu>
2022-09-24 13:18:26 -0700

    basic canvas 2d set up

---
+++ script.js
@@ -0,0 +1,5 @@
+//gets all canvas 2d drawing methods, stores the in canvas -> ctx variable.
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+
+console.log(ctx);
